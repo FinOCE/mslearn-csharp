40ed39c4d67b429ba3e60eb0a966b9c78e6b02d9
Finley Sherwood <me@itsf.in>
2021-07-19 01:14:56 +1000

    Complete write-first

---
+++ mslearn/1. write-first.cs
@@ -0,0 +1,13 @@
+// https://docs.microsoft.com/en-us/learn/modules/csharp-write-first/
+
+// Console.writeLine("Hello world!");
+
+Console.WriteLine("Congratulations!");
+Console.WriteLine(" ");
+Console.WriteLine("You wrote your first lines of code!");
+
+Console.WriteLine('b');
+
+Console.WriteLine(123);
+Console.WriteLine(12.3m);
+Console.WriteLine(true);
