8f8ae1d6b387131923c290e6900a65e6a6f324f5
Finley Sherwood <me@itsf.in>
2021-07-19 01:15:04 +1000

    Complete literals-variables

---
+++ mslearn/2. literals-variables.cs
@@ -0,0 +1,28 @@
+// https://docs.microsoft.com/en-us/learn/modules/csharp-literals-variables/
+
+string fullName;
+char userOption;
+decimal particlesPerMillion;
+bool processedCustomer;
+
+string firstName;
+firstName = "Finley";
+Console.WriteLine(firstName);
+firstName = "Fin";
+Console.WriteLine(firstName);
+
+string value = "Bob";
+Console.WriteLine(value);
+
+var message = "Hello world!";
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celcius.");
