e049d34cb1bdfa78a13f9117ce1ba29c6c453ed0
Finley Sherwood <me@itsf.in>
2021-07-28 20:01:14 +1000

    Complete choose-data-type

---
+++ csharp-data/choose-data-type/2. floating-point-types.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Floating point types:");
+
+Console.WriteLine($"float  : {float.MinValue} to {flat.MaxValue}");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue}");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue}");
---
+++ csharp-data/choose-data-type/3. reference-types.cs
@@ -0,0 +1,7 @@
+int[] data;
+data = new int[3];
+
+int[] data = new int[3];
+
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
