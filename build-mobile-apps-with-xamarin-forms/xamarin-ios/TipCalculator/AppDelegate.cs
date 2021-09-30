92416942c2ab2f900f3237faa30740b76a25c341
Finley Sherwood <me@itsf.in>
2021-09-30 23:18:09 +1000

    Complete xamarin-ios

---
+++ build-mobile-apps-with-xamarin-forms/xamarin-ios/TipCalculator/AppDelegate.cs
@@ -0,0 +1,48 @@
+﻿using Foundation;
+using UIKit;
+
+namespace TipCalculator
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register ("AppDelegate")]
+    public class AppDelegate : UIResponder, IUIApplicationDelegate {
+    
+        [Export("window")]
+        public UIWindow Window { get; set; }
+
+        [Export ("application:didFinishLaunchingWithOptions:")]
+        public bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+        {
+            // Create a new window instance based on the screen size
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            // Assign the root view controller
+            Window.RootViewController = new MyViewController();
+
+            // Make the window visible
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+
+        // UISceneSession Lifecycle
+
+        [Export ("application:configurationForConnectingSceneSession:options:")]
+        public UISceneConfiguration GetConfiguration (UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
+        {
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration.Create ("Default Configuration", connectingSceneSession.Role);
+        }
+
+        [Export ("application:didDiscardSceneSessions:")]
+        public void DidDiscardSceneSessions (UIApplication application, NSSet<UISceneSession> sceneSessions)
+        {
+            // Called when the user discards a scene session.
+            // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
+            // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        }
+    }
+}
+
---
+++ build-mobile-apps-with-xamarin-forms/xamarin-ios/TipCalculator/MyViewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+
+namespace TipCalculator
+{
+    class MyViewController : UIViewController
+    {
+        private UITextField totalAmount;
+        private UIButton calcButton;
+        private UILabel resultLabel;
+
+        public MyViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.View.BackgroundColor = UIColor.Yellow;
+            var topPadding = UIApplication.SharedApplication.Windows[0].SafeAreaInsets.Top;
+
+            totalAmount = new UITextField(new CGRect(20, 28 + topPadding, View.Bounds.Width - 40, 35))
+            {
+                KeyboardType = UIKeyboardType.DecimalPad,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Enter Total amount"
+            };
+
+            calcButton = new UIButton(UIButtonType.Custom)
+            {
+                Frame = new CGRect(20, 71 + topPadding, View.Bounds.Width - 40, 45),
+                BackgroundColor = UIColor.FromRGB(0, 0.5f, 0)
+            };
+            calcButton.SetTitle("Calculate", UIControlState.Normal);
+
+            resultLabel = new UILabel(new CGRect(20, 124 + topPadding, View.Bounds.Width - 40, 40))
+            {
+                TextColor = UIColor.Blue,
+                TextAlignment = UITextAlignment.Center,
+                Font = UIFont.SystemFontOfSize(24),
+                Text = "Tip is $0.00"
+            };
+
+            View.AddSubviews(totalAmount, calcButton, resultLabel);
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            calcButton.TouchUpInside += CalcButton_TouchUpInside;
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+
+            calcButton.TouchUpInside -= CalcButton_TouchUpInside;
+        }
+
+        void CalcButton_TouchUpInside(object sender, EventArgs e)
+        {
+            if (Double.TryParse(totalAmount.Text, out double value))
+            {
+                resultLabel.Text = string.Format("Tip is {0:C}", GetTip(value, 20));
+            }
+            else
+            {
+                resultLabel.Text = "Please enter a valid amount";
+            }
+
+            totalAmount.ResignFirstResponder();
+        }
+
+        public double GetTip(double amount, double percentage)
+        {
+            return amount * percentage / 100.0;
+        }
+    }
+}
